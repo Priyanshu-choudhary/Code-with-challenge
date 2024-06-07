8e48125c5ae52b1c5149129ca9e7fda6c13bb1c7
Priyanshu-choudhary <broyadi23@gmail.com>
2024-06-07 15:35:11 +0530

    complete demo upto login/sign and basic functionally

--- src/loginSigin/RegisterUser.jsx
+++ src/loginSigin/RegisterUser.jsx
@@ -26,7 +26,7 @@ function RegisterUser() {
     try {
       const response = await signUp(username, password);
       if (response.ok) {
-        navigate('/login');
+        navigate('/yourProfile');
       } else {
         console.error('Error registering:', response.statusText);
       }

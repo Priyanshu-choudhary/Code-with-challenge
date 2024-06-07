e6cb2de536e574f65330017e3d77ce1c583b70fd
Priyanshu-choudhary <broyadi23@gmail.com>
2024-06-07 15:30:05 +0530

    k

--- src/Profile/EditProfile.jsx
+++ src/Profile/EditProfile.jsx
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
+import CircularProgress from '@mui/material/CircularProgress';
 import { UserContext } from '../Context/UserContext';
 
 const ValidationTextFields = () => {
@@ -18,6 +19,7 @@ const ValidationTextFields = () => {
         name: user,
         password: password,
     });
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchUserData = async () => {
@@ -33,8 +35,10 @@ const ValidationTextFields = () => {
                 const data = await response.json();
                 setExistingData(data);
                 setFormData(data); // Initialize form data with existing data
+                setIsLoading(false);
             } catch (error) {
                 console.error('Error fetching user data:', error);
+                setIsLoading(false);
             }
         };
 
@@ -56,116 +60,115 @@ const ValidationTextFields = () => {
         const updatedData = {
             // ...existingData,
             ...formData,
-            
-
         };
 
         try {
             const url = 'https://testcfc-1.onrender.com/users';
-            updatedData.password=password;
+            updatedData.password = password;
 
             const headers = new Headers();
             headers.set('Content-Type', 'application/json');
             headers.set('Authorization', 'Basic ' + btoa(user + ":" + password));
 
- 
             const response = await fetch(url, {
                 method: 'PUT',
                 headers: headers,
-                body: JSON.stringify(updatedData)
+                body: JSON.stringify(updatedData),
             });
 
             if (response.ok) {
-                console.log('User created successfully:');
-                alert("Sucessful updated....!");
+                console.log('User updated successfully:');
+                alert("Successfully updated!");
                 location.reload();
             }
         } catch (error) {
-            console.log('error  unsuccessfully:' + error);
-            alert("Unsucessful....!");
+            console.log('Error updating user:', error);
+            alert("Unsuccessful update!");
         }
-
-
-     
-
     };
 
     return (
-        <Box
-            component="form"
-            sx={{
-                '& .MuiTextField-root': { m: 1, width: '25ch' },
-            }}
-            noValidate
-            autoComplete="on"
-            onSubmit={handleSubmit}
-        >
-            <div>
-                <TextField
-                    id="standard-required"
-                    label="Email"
-                    name="email"
-                    variant="standard"
-                    value={formData.email}
-                    onChange={handleChange}
-                />
-                <TextField
-                    id="standard-required"
-                    label="Phone Number"
-                    name="number"
-                    type="number"
-                    variant="standard"
-                    value={formData.number}
-                    onChange={handleChange}
-                />
-                <TextField
-                    id="standard-required"
-                    label="Links"
-                    name="links"
-                    variant="standard"
-                    value={formData.links}
-                    onChange={handleChange}
-                />
-                <TextField
-                    id="standard-required"
-                    label="Branch"
-                    name="branch"
-                    variant="standard"
-                    value={formData.branch}
-                    onChange={handleChange}
-                />
-                <TextField
-                    id="standard-required"
-                    label="Year"
-                    name="year"
-                    type="number"
-                    variant="standard"
-                    value={formData.year}
-                    onChange={handleChange}
-                />
-                <TextField
-                    id="standard-required"
-                    label="Collage"
-                    name="collage"
-                    variant="standard"
-                    value={formData.collage}
-                    onChange={handleChange}
-                />
-                <TextField
-                    id="standard-multiline-static"
-                    label="Skills"
-                    name="skills"
-                    multiline
-                    rows={3}
-                    variant="standard"
-                    value={formData.skills}
-                    onChange={handleChange}
-                />
-                <Button style={{ marginTop: "18%" }} variant="contained" endIcon={<SendIcon />} type="submit">
-                    Send
-                </Button>
+        isLoading ? (
+            <div className="loading-screen">
+                <CircularProgress />
             </div>
-        </Box>
+        ) : (
+            <Box
+                component="form"
+                sx={{
+                    '& .MuiTextField-root': { m: 1, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="on"
+                onSubmit={handleSubmit}
+            >
+                <div>
+                    <TextField
+                        id="standard-required"
+                        label="Email"
+                        name="email"
+                        variant="standard"
+                        value={formData.email}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        id="standard-required"
+                        label="Phone Number"
+                        name="number"
+                        type="number"
+                        variant="standard"
+                        value={formData.number}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        id="standard-required"
+                        label="Links"
+                        name="links"
+                        variant="standard"
+                        value={formData.links}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        id="standard-required"
+                        label="Branch"
+                        name="branch"
+                        variant="standard"
+                        value={formData.branch}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        id="standard-required"
+                        label="Year"
+                        name="year"
+                        type="number"
+                        variant="standard"
+                        value={formData.year}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        id="standard-required"
+                        label="Collage"
+                        name="collage"
+                        variant="standard"
+                        value={formData.collage}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        id="standard-multiline-static"
+                        label="Skills"
+                        name="skills"
+                        multiline
+                        rows={3}
+                        variant="standard"
+                        value={formData.skills}
+                        onChange={handleChange}
+                    />
+                    <Button style={{ marginTop: "18%" }} variant="contained" endIcon={<SendIcon />} type="submit">
+                        Send
+                    </Button>
+                </div>
+            </Box>
+        )
     );
 };
 

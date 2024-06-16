9d854a23670f0eb5842fc3c388857e1b8693e202
Priyanshu-choudhary <broyadi23@gmail.com>
2024-06-16 23:47:04 +0530

    mcq mechanism add

--- src/loginSigin/SignUpApi.jsx
+++ src/loginSigin/SignUpApi.jsx
@@ -1,11 +1,13 @@
 async function signUp(username, password, email) {
   try {
+    const roles = ["USER"]; // Assuming you want every user to have the role "USER"
+
     const response = await fetch('https://testcfc.onrender.com/Public/Create-User', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ name: username, password: password, email: email,roles:"USER" }), // Include email in the request body
+      body: JSON.stringify({ name: username, password: password, email: email, roles: roles }),
     });
 
     return response;

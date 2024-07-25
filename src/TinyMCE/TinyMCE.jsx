e2941441199c9ac6d6bbb7dfd49c37186591c616
Priyanshu-choudhary <broyadi23@gmail.com>
2024-07-26 01:13:30 +0530

    swift to tinyMCE 3

--- src/TinyMCE/TinyMCE.jsx
+++ src/TinyMCE/TinyMCE.jsx
@@ -1,25 +1,4 @@
-import React, { useEffect } from 'react';
 
-const Tinymce = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = '/src/tinymceLib/js/tinymce/tinymce.min.js';
-    script.onload = () => {
-      window.tinymce.init({
-        selector: '#mytextarea',
-      });
-    };
-    document.body.appendChild(script);
-  }, []);
-
-  return (
-    <div>
-      <textarea id="mytextarea">Hello, World!</textarea>
-    </div>
-  );
-};
-
-export default Tinymce;
 
 // import { useRef, useEffect } from 'react';
 // import { Editor } from '@tinymce/tinymce-react';
@@ -118,74 +97,75 @@ export default Tinymce;
 //         </div>
 //     );
 // }
-// import { useRef, useState, useEffect } from 'react';
-// import FroalaEditor from 'react-froala-wysiwyg';
-// import 'froala-editor/js/plugins.pkgd.min.js';
-// import 'froala-editor/js/froala_editor.pkgd.min.js';
-// import 'froala-editor/css/froala_editor.pkgd.min.css';
-// import 'froala-editor/css/froala_style.min.css';
 
-// export default function Tinymce({ setDescription, initialValue }) {
-//     const editorRef = useRef(null);
-//     const [content, setContent] = useState(initialValue ? initialValue : "<p>Write your Description here.</p>");
+import { useRef, useState, useEffect } from 'react';
+import FroalaEditor from 'react-froala-wysiwyg';
+import 'froala-editor/js/plugins.pkgd.min.js';
+import 'froala-editor/js/froala_editor.pkgd.min.js';
+import 'froala-editor/css/froala_editor.pkgd.min.css';
+import 'froala-editor/css/froala_style.min.css';
 
-//     const handleModelChange = (newContent) => {
-//         setContent(newContent);
-//         setDescription(newContent);
-//         // console.log(newContent);
-//     };
+export default function Tinymce({ setDescription, initialValue }) {
+    const editorRef = useRef(null);
+    const [content, setContent] = useState(initialValue ? initialValue : "<p>Write your Description here.</p>");
 
-//     useEffect(() => {
-//         setContent(initialValue);
-//     }, [initialValue]);
+    const handleModelChange = (newContent) => {
+        setContent(newContent);
+        setDescription(newContent);
+        // console.log(newContent);
+    };
 
-//     return (
-//         <FroalaEditor
-//             tag='textarea'
-//             model={content}
-//             onModelChange={handleModelChange}
-//             config={{
-//                 ref: editorRef,
-//                 height: 500,
-//                 toolbarButtons: [
-//                     'bold', 'italic', 'underline', 'paragraphFormat', 'alert',
-//                     'align', 'formatOL', 'formatUL', 'indent', 'outdent',
-//                     'insertLink', 'insertImage', 'insertTable', 'emoticons', 'fontFamily', 'fontSize', 'color', 
-//                     'specialCharacters', 'fullscreen', 'html', 'save', 'print', 'spellChecker', 'search', 'undo', 'redo'
-//                 ],
-//                 pluginsEnabled: [
-//                     'align', 'codeBeautifier', 'codeView', 'colors', 'draggable', 'emoticons', 'fontAwesome', 'image', 'link', 'lists',
-//                     'paragraphFormat', 'quote', 'save', 'spellChecker', 'table', 'url', 'wordPaste'
-//                 ],
-//                 // Enable base64 image upload
-//                 imageUploadToBase64: true,
-//                 // Custom image upload handler
-//                 events: {
-//                     'image.beforeUpload': function (files) {
-//                         // Create a FileReader object
-//                         const reader = new FileReader();
+    useEffect(() => {
+        setContent(initialValue);
+    }, [initialValue]);
 
-//                         // Set the onload callback
-//                         reader.onload = (e) => {
-//                             // Get the base64 string
-//                             const base64 = e.target.result;
+    return (
+        <FroalaEditor
+            tag='textarea'
+            model={content}
+            onModelChange={handleModelChange}
+            config={{
+                ref: editorRef,
+                height: 500,
+                toolbarButtons: [
+                    'bold', 'italic', 'underline', 'paragraphFormat', 'alert',
+                    'align', 'formatOL', 'formatUL', 'indent', 'outdent',
+                    'insertLink', 'insertImage', 'insertTable', 'emoticons', 'fontFamily', 'fontSize', 'color', 
+                    'specialCharacters', 'fullscreen', 'html', 'save', 'print', 'spellChecker', 'search', 'undo', 'redo'
+                ],
+                pluginsEnabled: [
+                    'align', 'codeBeautifier', 'codeView', 'colors', 'draggable', 'emoticons', 'fontAwesome', 'image', 'link', 'lists',
+                    'paragraphFormat', 'quote', 'save', 'spellChecker', 'table', 'url', 'wordPaste'
+                ],
+                // Enable base64 image upload
+                imageUploadToBase64: true,
+                // Custom image upload handler
+                events: {
+                    'image.beforeUpload': function (files) {
+                        // Create a FileReader object
+                        const reader = new FileReader();
 
-//                             // Insert the image into the editor
-//                             this.image.insert(base64, null, null, this.image.get(), files[0]);
+                        // Set the onload callback
+                        reader.onload = (e) => {
+                            // Get the base64 string
+                            const base64 = e.target.result;
 
-//                             // Prevent the default upload
-//                             return false;
-//                         };
+                            // Insert the image into the editor
+                            this.image.insert(base64, null, null, this.image.get(), files[0]);
 
-//                         // Read the file as a data URL (base64)
-//                         reader.readAsDataURL(files[0]);
+                            // Prevent the default upload
+                            return false;
+                        };
 
-//                         // Prevent the default upload
-//                         return false;
-//                     }
-//                 },
-//             }}
-//         />
-//     );
-// }
+                        // Read the file as a data URL (base64)
+                        reader.readAsDataURL(files[0]);
+
+                        // Prevent the default upload
+                        return false;
+                    }
+                },
+            }}
+        />
+    );
+}
 

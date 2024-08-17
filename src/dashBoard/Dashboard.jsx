d63ab3ab5f70a44105fe0ece5af01681c8b72650
Hem <hcjha05@gmail.com>
2024-08-17 16:52:46 +0530

    adding Document

--- src/dashBoard/Dashboard.jsx
+++ src/dashBoard/Dashboard.jsx
@@ -31,6 +31,7 @@ const navigation = [
     { name: 'Editor', href: '/EditorComponent', current: false },
     { name: 'Problems', href: '/data', current: false },
     { name: 'Contest', href: '/contest', current: false },
+    { name: 'Document', href: '/', current : false },
 ]
 
 // User menu items
